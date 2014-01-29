5f547255e4b229f1600171d55fcd7ea6fcea6071
Nicolas Barbotte <n.barbotte@gmail.com>
2014-01-29 16:35:05 +0100

    min

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,18 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     less: {
       production: {
+        options: {
+          compress: true
+        },
         files: {
           'public/css/style.css': 'public/css/style.less'
+        }
+      }
+    },
+    uglify: {
+      production: {
+        files: {
+          'public/scripts/main.min.js': 'public/scripts/main.js'
         }
       }
     },
@@ -17,16 +27,21 @@ module.exports = function (grunt) {
       less: {
         files: ['**/*.less'],
         tasks: ['less']
+      },
+      uglify: {
+        files: ['public/scripts/main.js'],
+        tasks: ['uglify']
       }
     }
   });
 
   // Load tasks.
 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Task definition.
-  grunt.registerTask('default', ['less']);
+  grunt.registerTask('default', ['less', 'uglify']);
 
 };
